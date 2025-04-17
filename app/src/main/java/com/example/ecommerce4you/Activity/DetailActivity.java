8140a04ac18d4576f32c380ea5bd5df73d76ce6f
DorraBenMimoun <dorrabenmimoun@gmail.com>
2025-04-17 19:27:01 +0100

    Item Detail Backend ,color & size of item

--- app/src/main/java/com/example/ecommerce4you/Activity/DetailActivity.java
+++ app/src/main/java/com/example/ecommerce4you/Activity/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce4you.Activity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,20 +8,76 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.bumptech.glide.Glide;
+import com.example.ecommerce4you.Adapter.ColorAdapter;
+import com.example.ecommerce4you.Adapter.PicListAdapter;
+import com.example.ecommerce4you.Adapter.SizeAdapter;
+import com.example.ecommerce4you.Domain.ItemsModel;
+import com.example.ecommerce4you.Helper.ManagmentCart;
 import com.example.ecommerce4you.R;
+import com.example.ecommerce4you.databinding.ActivityDetailBinding;
+
+import java.util.ArrayList;
 
 public class DetailActivity extends AppCompatActivity {
+    private ActivityDetailBinding binding;
+    private ItemsModel object;
+    private int numberOrder=1;
+    private ManagmentCart managmentCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding =ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        managmentCart = new ManagmentCart(this);
+
+        getBundles();
+        initPicList();
+        initSize();
+        initColor();
+
+    }
+
+    private void initColor() {
+        binding.recylclerColor.setAdapter(new ColorAdapter(object.getColor()));
+        binding.recylclerColor.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,true));
+    }
+
+    private void initSize() {
+        binding.recylerSize.setAdapter(new SizeAdapter(object.getSize()));
+        binding.recylerSize.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,true));
+    }
+
+    private void initPicList() {
+        ArrayList<String>picList = new ArrayList<>(object.getPicUrl());
+
+        Glide.with(this)
+                .load(picList.get(0))
+                .into(binding.pic);
+
+        binding.picList.setAdapter(new PicListAdapter(picList,binding.pic));
+        binding.picList.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+    }
+
+    private void getBundles() {
+        object= (ItemsModel) getIntent().getSerializableExtra("object");
+        binding.titleTxt.setText(object.getTitle());
+        binding.priceTxt.setText("$"+object.getPrice());
+        binding.oldPriceTxt.setText("$"+object.getOldPrice());
+        binding.oldPriceTxt.setPaintFlags(binding.oldPriceTxt.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        binding.descriptionTxt.setText(object.getDescription());
+
+        binding.addToCartBtn.setOnClickListener(v->
+        {
+            object.setNumberinCart(numberOrder);
+            managmentCart.insertItem(object);
         });
+
+        binding.backBtn.setOnClickListener(v->finish());
     }
 }
--- app/src/main/java/com/example/ecommerce4you/Adapter/PopularAdapter.java
+++ app/src/main/java/com/example/ecommerce4you/Adapter/PopularAdapter.java
@@ -1,6 +1,7 @@
 package com.example.ecommerce4you.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CenterInside;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.ecommerce4you.Activity.DetailActivity;
 import com.example.ecommerce4you.Domain.ItemsModel;
 import com.example.ecommerce4you.databinding.ViewholderPopularBinding;
 
@@ -50,6 +52,11 @@ public class PopularAdapter extends RecyclerView.Adapter<PopularAdapter.ViewHold
                 .apply(options)
                 .into(holder.binding.pic);
 
+        holder.itemView.setOnClickListener(v->{
+            Intent intent = new Intent(context, DetailActivity.class);
+            intent.putExtra("object",items.get(position));
+            context.startActivity(intent);
+        });
     }
 
     @Override
